18a494a2455a5a7e45671c07872c70cf6615bf45
Athiqur01 <athik.apece@gmail.com>
2024-08-16 01:36:45 +0600

    pagination

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -6,11 +6,25 @@ const Home = () => {
 
     const {count}=useLoaderData()
     console.log(count)
+    const itemsPerPage=6
+    const totalPages=Math.ceil(count/itemsPerPage)
+    console.log(totalPages)
+    const pages=[]
+    for(let i=0; i<totalPages; i++){
+        pages?.push(i)
+
+    }
+    console.log(pages)
 
     return (
         <div>
             <h2 className='text-4xl lg:text-6xl font-bold py-10'>Product</h2>
             <Product></Product>
+            <div className='space-x-3'>
+                {pages.map(page=><>
+                                   <button className='px-3 py-1 border-red-50 border-2 bg-red-50 '>{page}</button>
+                                </>)}
+            </div>
         </div>
     );
 };
